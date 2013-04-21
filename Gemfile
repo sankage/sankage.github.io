c7637877cbd9aeb147cb37bee3d7fcbdaeb615f5
David Briggs <godamonra@gmail.com>
2013-04-20 22:03:21 -0400

    update jekyll to match github requirements

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
 source 'https://rubygems.org'
 
+# GitHub requirements
 gem 'jekyll',     '=1.0.0'
 gem 'liquid',     '=2.5.0'
 gem 'redcarpet',  '=2.2.2'
+
+# Local requirements
 gem 'sass'
 gem 'rake'
--- _plugins/sass_converter.rb
+++ _plugins/sass_converter.rb
@@ -1,9 +1,6 @@
 module Jekyll
-  # Sass plugin to convert .scss to .css
-  # 
-  # Note: This is configured to use the new css like syntax available in sass.
-  require 'sass'
   class SassConverter < Converter
+    require 'sass'
     safe true
     priority :low
 
